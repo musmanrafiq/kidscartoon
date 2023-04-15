614925b865dd5a3eacd0119397afa4ee81ed31cb
Usman <usman.solemn@gmail.com>
2023-04-15 05:34:41 +0500

    updates

--- src/App.js
+++ src/App.js
@@ -5,19 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      <video 
+      width={100}
+      src='https://kidscartoon.s3.ap-south-1.amazonaws.com/Y2Mate.is%20-%20Zaky%27s%20Ramadan%20-%20Suhoor%20%26%20Tahajjud%20%28Islamic%20Cartoon%29-S60YaYe84XE-720p-1655348516357.mp4'
+      controls>Your browser not support video tag</video>
     </div>
   );
 }
