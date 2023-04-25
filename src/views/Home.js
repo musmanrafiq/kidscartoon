fad111cefcbd000a05ef6d29a312b24ba535288e
Usman <usman.solemn@gmail.com>
2023-04-25 06:00:37 +0500

    Removed test code

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,30 +1,11 @@
 import React from "react";
-import db from "../Firebase";
-import { addDoc, collection } from "@firebase/firestore"
 
 class Home extends React.Component
  {
   constructor(){
     super();
   }
-
-  add = () => {
-
-const ref = collection(db, "videos") // Firebase creates this automatically
-let data = {
-testData: '111'
-}
-try {
-addDoc(ref, data)
-} catch(err) {
-console.log(err)
-}
-  };
-  
   render(){
-
-  this.add();
-
   return <h1>Home</h1>;
   }
 }
